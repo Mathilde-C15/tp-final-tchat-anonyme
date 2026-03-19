606e3e2d38137e060f0c075eb362832440e219f4
Mathilde Cotelle <mathildecotelle@orange.fr>
2026-03-19 12:28:50 +0100

    Creation connexion database, et creation des routes

---
+++ configs/settings.php
@@ -0,0 +1,29 @@
+<?php
+
+const AVAILABLE_ROUTES = [
+    '/' => [
+        'controller' => 'RoomController',
+        'method' => 'index'
+    ],
+    'room' => [
+        'controller' => 'RoomController',
+        'method' => 'index'
+    ],
+    'about' => [
+        'controller' => 'AboutController',
+        'method' => 'index'
+    ],
+    'add_room' => [
+        'controller' => 'RoomController',
+        'method' => 'index'
+    ],
+    'pin_message' => [
+        'controller' => 'MessageController',
+        'method' => 'index'
+    ],
+    'add_message' => [
+        'controller' => 'MessageController',
+        'method' => 'index'
+    ]
+
+];
---
+++ services/DataBase.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Services;
+
+use PDO;
+
+class Database
+{
+    // Attributs privés
+    private string $host = "localhost";
+    private string $dbname = "tp-final-tchat-anonyme";
+    private string $username = "root";
+    private string $password = "";
+    private ?PDO $pdo = null;
+
+    // Constructeur
+    public function __construct()
+    {
+        try {
+            $this->pdo = new PDO(
+                "mysql:host={$this->host};dbname={$this->dbname};charset=utf8",
+                $this->username,
+                $this->password
+            );
+
+            // Activation des erreurs PDO
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            die("Erreur de connexion : " . $e->getMessage());
+        }
+    }
+
+    // Méthode publique pour récupérer la connexion
+    public function getConnection(): PDO
+    {
+        return $this->pdo;
+    }
+}
+
+// $bd = new Database();
+// var_dump($bd-> getConnection());
+
+
+
+
