9c8b02e7f4056841047b2568730b440ccbdd18df
Mathilde Cotelle <mathildecotelle@orange.fr>
2026-03-20 15:04:31 +0100

    getters et setters des messages incomplet, manque set et get pinned

---
+++ models/Message.php
@@ -0,0 +1,63 @@
+<?php
+
+class Message {
+    private int $id;
+    private string $content;
+    private int $date;
+    private int $pinned;
+
+    public function __construct(int $id, string $content, int $date, int $pinned){
+        $this->id = $id;
+        $this->content = $content;
+        $this->date = $date;
+        $this->pinned = $pinned;
+    }
+
+    // Getters
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getContent(){
+        return $this->content;
+    }
+
+    public function getDate(){
+        return $this->date;
+    }
+
+    public function getPinned(){
+        return $this->pinned;
+    }
+
+    // Setters
+
+    public function setId($id)
+    {
+        if (!preg_match('/^[0-9]+$/', $id)) {
+            throw new \Exception("ID invalide");
+        }
+
+        $this->id = (int) $id;
+    }
+
+    public function setContent(string $content): string{
+        if (strlen(trim($content)) < 1) {
+            throw new \Exception("Le contenu doit contenir au moins 1 caractère");
+        }
+
+        $this->content = trim($content);
+    }
+
+    public function setDate($date)
+    {
+        // Format attendu : YYYY-MM-DD HH:MM:SS
+        if (!preg_match('/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/', $created_at)) {
+            throw new \Exception("Format de date invalide");
+        }
+
+        $this->date = $date;
+    }
+
+}
