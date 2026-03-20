0328fff6804fd47eb88c03e5815ed8fc1ea720ad
Mathilde Cotelle <mathildecotelle@orange.fr>
2026-03-20 15:42:34 +0100

    remplissage RoomRepository. Récupérer toutes les rooms et une seule room

--- repositories/RoomRepository.php
+++ repositories/RoomRepository.php
@@ -13,4 +13,41 @@ class RoomRepository{
         $db = new DataBase();
         $this->db = $db->getConnection();
     }
+
+    // retourne toutes les rooms
+    public function findRooms(){
+        $stmt = $this->pdo->query("SELECT * FROM room ORDER BY name DESC");
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $rooms = [];
+
+        foreach ($rows as $row) {
+            $room = new Post();
+            $room->setId($row['id']);
+            $room->setName($row['name']);
+            
+            $room[] = $room;
+        }
+
+        return $room;
+    }
+
+    // retourne une seule room
+    public function findOneRoomById($id)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM room WHERE id = ?");
+        $stmt->execute([$id]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$row) {
+            return null; // room non trouvé
+        }
+
+        $room = new Room();
+        $room->setId($row['id']);
+        $room->setName($row['name']);
+
+        return $room;
+    }
+
 }
